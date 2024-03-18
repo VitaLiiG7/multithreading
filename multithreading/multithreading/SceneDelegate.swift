80a11e91d7823cdfa527f73718a66c82bbb34266
Vitqqqqqqqq <130509045+Vitqqqqqqqq@users.noreply.github.com>
2024-03-19 01:12:10 +0300

    Решил задачи 1 task

--- multithreading/multithreading/SceneDelegate.swift
+++ multithreading/multithreading/SceneDelegate.swift
@@ -1,22 +1,20 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        createRootViewController(windowScene)
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
+    private func createRootViewController(_ windowScene: UIWindowScene) {
+        let mainViewController = ViewControllerTwo()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+    }
 }
 
--- multithreading/multithreading/ViewController.swift
+++ multithreading/multithreading/ViewController.swift
@@ -2,10 +2,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+/// Задача разгрузить главный поток 
+final class ViewController: UIViewController {
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        Thread.detachNewThread {
+            for _ in (0..<10) {
+                let currentThread = Thread.current
+                print("1, Current thread: \(currentThread)")
+            }
+        }
+            for _ in (0..<10) {
+                let currentThread = Thread.current
+                print("2, Current thread: \(currentThread)")
+        }
     }
 }
-
